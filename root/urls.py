98f2492c784138dfde4e7ab8a33f5d6ac39e66ea
rashideveloper <bubbless7456@gmail.com>
2023-01-18 01:36:22 +0500

    sign up

--- root/urls.py
+++ root/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('users.urls')),
+    path('chats/', include('chats.urls')),
     path('admin/', admin.site.urls),
-    path('', include('chats.urls')),
 ]
--- users/urls.py
+++ users/urls.py
@@ -1,2 +1,7 @@
+from django.urls import path
+
+from users.views.auth import AuthUserSignUpView
+
 urlpatterns = [
+    path('auth/signup/', AuthUserSignUpView.as_view(), name='auth_signup')
 ]
