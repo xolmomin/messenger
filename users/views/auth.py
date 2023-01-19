cc5c6a205dd90c92538f4c46eb2cd1a0d0b04a76
mirsaid <mirsaidavazbekov@gmail.com>
2023-01-19 16:37:04 +0500

    update signup view

--- users/views/auth.py
+++ users/views/auth.py
@@ -1,9 +1,20 @@
-from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, CreateView
+
+from users.forms import SignUpForm
+from users.models import User
 
 
 #  TODO: Editing later / because i can't add ""Device model"" / users add manually
-class UserSignUpView(TemplateView):
+class UserSignUpView(CreateView):
     template_name = 'users/auth/signup.html'
+    form_class = SignUpForm
+    model = User
+    success_url = reverse_lazy('auth_signin')
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
 
 
 class UserSignInView(TemplateView):
